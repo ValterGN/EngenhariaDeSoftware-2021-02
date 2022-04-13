231419ee0fc2da1513bd22268ca633abc84f189f
rock051 <fernando.roque@estudante.ufla.br>
2022-04-13 13:10:19 -0300

    Implementação do Conection.php

--- BManager/Model/Connection.php
+++ BManager/Model/Connection.php
@@ -1,25 +1,39 @@
 <?php
 
 class Connection{
-    private $serverName = "";
-    private $userName = "";
+    private $serverName = "localhost";
+    private $userName = "root";
     private $password = "";
-    private $db = "";
+    private $db = "BD_BMnager";
+    private $conn = null;   
 
     private function abreConexao(){
-        
+        if ($this->conn==null){
+            $this->conn = new mysqli($this->serverName , $this->userName ,$this->password ,$this->db);
+            //echo "conexão feita com sucesso";
+        }
+        if (! $this->conn){
+            //die("fallhou a conexão:" .$conn->connect_error);
+        }    
+    
     }
 
     private function encerraConexao(){
-
+        $this->conn->close();
     }
 
     public function executaQuery($sql){
-
+        $this->abreConexao();
+        $this->conn->query($sql);
+        $this->encerraConexao();
     }
 
     public function retornaSelect($sql){
-        return "retorno de select";
+        $this->abreConexao();
+        $resultado = $this->conn->query($sql);
+        $this->encerraConexao(); 
+        
+        return $resultado;
     }
 }
 
