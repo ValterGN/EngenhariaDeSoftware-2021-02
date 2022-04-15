1ad8b00983561f0f5d8af1058cdf1828d0f8460e
Claudio Reis <100233495+claudioreisjrs@users.noreply.github.com>
2022-04-15 14:57:39 -0300

    Estilização das páginas LeitorCRUD e ListarLeitores e adição da navbar
    Estilização das páginas LeitorCRUD e ListarLeitores e adição da navbar

--- BManager/View/LeitorCRUD.php
+++ BManager/View/LeitorCRUD.php
@@ -1,15 +1,36 @@
-<form action="../Controller/LeitorController.php" method="POST">
-    <input type="text" name="funcao" id="funcao" value="nada" hidden>
+<!--class LeitorCrud  -->
 
-    <label>Id: </label> <input type="text" name="txtCodigo" id="txtCodigo"> <br>
-    <label>Nome: </label> <input type="text" name="txtNome" id="txtNome"> <br>
-    <label>CPF: </label> <input type="text" name="txtCpf" id="txtCpf"> <br>
-    <label>Data de nascimento: </label> <input type="date" name="txtDataNasc" id="txtDataNasc"> <br>
 
-    <button name="btnSalvar" id="btnSalvar">Salvar</button>
-    <button name="btnAlterar" id="btnAlterar">Alterar</button>
-    <button name="btnExcluir" id="btnExcluir">Excluir</button>
+<head>
+	<!-- Estilo da navbar utilizando bootstrap4 -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<!-- Deixa navbar no modo responsivo -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	
+	<link rel="stylesheet" type="text/css"  href="../css/estilo.css">	
+	<title>Cadastro Leitores</title>
 
+</head>
+
+<body>
+<?php include 'menu.php'; ?>
+</body>
+
+<form action="../Controller/LeitorController.php" method="POST" class="formulario">
+    <legend id="txtTitulo">Cadastro de Leitores </legend>
+	
+    <label>Id: </label> <input type="text" name="txtCodigo" id="txtCodigo" class="txt"> <br>
+    <label>Nome: </label> <input type="text" name="txtNome" id="txtNome" class="txt"> <br>
+    <label>CPF: </label> <input type="text" name="txtCpf" id="txtCpf" class="txt"> <br>
+    <label>Nascimento: </label> <input type="date" name="txtDataNasc" id="txtDataNasc" class="txt"> <br>
+
+    <button name="btnSalvar"  id="btnSalvar"  class="botao"> <i class="fa fa-check"></i> Salvar</button>
+    <button name="btnAlterar" id="btnAlterar" class="botao"> <i class="fa fa-pencil"></i> Alterar</button>
+    <button name="btnExcluir" id="btnExcluir" class="botao"> <i class="fa fa-trash"></i> Excluir</button>
 </form>
 
 <script>
--- BManager/View/ListarLeitor.php
+++ BManager/View/ListarLeitor.php
@@ -1,2 +1,82 @@
-<h1>ListarLeitor: </h1>
-<?php echo($listaLeitores)?>
+<head>
+	<!-- Estilo da navbar utilizando bootstrap4 -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<!-- Deixa navbar no modo responsivo -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link rel="stylesheet" type="text/css"  href="../css/estilo.css">	
+	<title>Listar Leitor</title>
+
+</head>
+
+<body>
+<!--Inclusao do menu.php com include -->	
+<?php include 'menu.php'; ?>
+</body>
+
+
+<legend id="txtListarLeitor">Leitores</legend>
+
+
+<!--teste chamar busca de leitor controller -->
+
+
+
+
+<!-- fim teste busca -->
+
+
+<form >
+	<center><input type="text" name="txtBusca" class="txtBusca"></input>
+	<button>Buscar</button>
+	
+	<form action="https://google.com"><button> Inserir </button> </form>
+	<br>
+	<br>
+	<br>
+
+</form>
+
+
+
+<?php
+
+
+echo"<table>
+		<tr>
+			<th>Código</th>	
+			<th>Nome</th>
+			<th>CPF</th>
+			<th>Nascimento</th>
+		
+		
+		
+		</tr>";
+	
+
+
+while ($row = $listaLeitores->fetch_assoc()) {	
+    echo "<tr>";
+    echo "<td>" .$row["ID"]. "</td>".
+		 "<td>" .$row["Nome"]. "</td>".
+		 "<td>" .$row["CPF"]. "</td>".
+		 "<td>" .$row["DataNascimento"]. "</td>".
+	 
+		/* "<td> $botaoEdit </td>".*/
+		"<td style='background-color: white'> <form action='https://google.com'>  <button type='submit' class='botaoEditar'>  <i class='fa fa-pencil'></i> </button> </form> </td>".
+		"<td style='background-color: white'> <form action='https://google.com'>  <button type='submit' class='botaoExcluir'>  <i class='fa fa-remove'></i> </button> </form> </td>";
+		
+		
+	echo "</tr>";
+}
+
+
+		
+	echo "</table>";
+/*<?php echo $botaoEdit ?>*/
+?>
+
+
